2f8a667cb0579535b868c0389bd4b7c9d8c35d51
emilio <emiliorizzo@gmail.com>
2017-12-31 17:04:47 -0300

    Add fields

---
+++ src/config/fields.js
@@ -0,0 +1,14 @@
+export default {
+  block: {
+    icon: 'block',
+    key: 'number'
+  },
+  timestamp: {
+    icon: 'stopwatch',
+    filters: []
+  },
+  transaction: {
+    key: '',
+    icon: 'credit-card'
+  }
+}
