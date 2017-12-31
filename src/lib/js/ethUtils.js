08f9729c4c9e647ecda69cfa5a0814c1036fd9b7
emilio <emiliorizzo@gmail.com>
2017-12-31 17:04:38 -0300

    Add ethutils

---
+++ src/lib/js/ethUtils.js
@@ -0,0 +1,32 @@
+export const isStrictAddress = address => {
+  return /^0x[0-9a-f]{40}$/i.test(address)
+}
+
+export const isAddress = address => {
+  if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
+    // check if it has the basic requirements of an address
+    return false
+  } else if (
+    /^(0x)?[0-9a-f]{40}$/.test(address) ||
+    /^(0x)?[0-9A-F]{40}$/.test(address)
+  ) {
+    // If it's all small caps or all all caps, return true
+    return true
+  } else {
+    // Otherwise check each case
+    return false
+  }
+}
+
+export const isTransaction = tx => {
+  if (!/^(0x)?[0-9a-f]{64}$/i.test(tx)) {
+    // check if it has the basic requirements of an address
+    return false
+  } else if (/^(0x)?[0-9a-f]{64}$/.test(tx) || /^(0x)?[0-9A-F]{64}$/.test(tx)) {
+    // If it's all small caps or all all caps, return true
+    return true
+  } else {
+    // Otherwise check each case
+    return false
+  }
+}
