8e28674f8b0263ce6c6e6dc8339ff3adf21547a7
emilio <emiliorizzo@gmail.com>
2018-01-03 13:50:19 -0300

    Update Tokens list, add message for empty results

--- src/components/Tokens.vue
+++ src/components/Tokens.vue
@@ -1,27 +1,25 @@
 <template lang="pug">
   #tokens
     h2 erc20 tokens
-    template(v-for='token in tokens')
-      .box.token
-        ul.plain
-          li 
-            h2 {{ token.name }}
-          //-li {{ token.shortName }}
-          li 
-            router-link(:to='"/tokens/" + token._id + "/events"') 
-              span Events: {{token.Events}}
-          li 
-            router-link(:to='"/tokens/" + token._id + "/accounts"') Accounts
+    template(v-if='tokens.length')
+      template(v-for='token in tokens')
+        .box.token
+          ul.plain
+            li 
+              h2 {{ token.name }}
+            //-li {{ token.shortName }}
+            li 
+              router-link(:to='"/tokens/" + token._id + "/events"') 
+                span Events: {{token.Events}}
+            li 
+              router-link(:to='"/tokens/" + token._id + "/accounts"') Accounts
+    template(v-else)
+      h1.big.gray There are no tokens yet
 </template>
 <script>
 import { mapState } from 'vuex'
 export default {
   name: 'Tokens',
-  data () {
-    return {
-
-    }
-  },
   computed: {
     ...mapState({
       tokens: state => state.backend.tokens
@@ -31,11 +29,12 @@ export default {
 }
 </script>
 <style lang="stylus">
- @import '../lib/styl/vars.styl'
+  @import '../lib/styl/vars.styl'
+
   .token
     display inline-flex
     min-width 18em
-    flex 1 
+    flex 1
     padding 0em 3em 3em
 </style>
 
