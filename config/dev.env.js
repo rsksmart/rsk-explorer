44e737d5cdff621c04ed1768b493ca854d595a9e
emilio <emiliorizzo@gmail.com>
2018-03-20 09:02:48 -0300

    Update config

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,8 +2,10 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 var cfg = require('../src/config/config.json')
+cfg = cfg.dev
+
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WS_URL: '"ws://192.168.56.103:3003"'
+  WS_URL: '"' + cfg.WS_URL + '"',
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,6 +1,7 @@
 'use strict'
 var pkg = require('../package.json')
 var cfg = require('../src/config/config.json')
+cfg = cfg.prod
 module.exports = {
   NODE_ENV: '"production"',
   WS_URL: '"' + cfg.WS_URL + '"',
