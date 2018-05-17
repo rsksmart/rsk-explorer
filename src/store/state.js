32974083974645a3f08dc155f7c0a25e25056f2d
emi <emiliorizzo@gmail.com>
2018-05-17 15:24:08 -0300

    Add media breakpoints to state

--- src/store/state.js
+++ src/store/state.js
@@ -1,11 +1,12 @@
 import { colors, blocksColors } from '../lib/js/colors.js'
 import menuItems from '../lib/js/menuItems.js'
-
+import mediaBreakpoints from '../config/media_breakpoints.json'
 export default function () {
   return {
     loadingConfig: null,
     APP: process.env.APP,
     menuItems,
+    mediaBreakpoints,
     date: Date.now(),
     dateInterval: null,
     size: {
