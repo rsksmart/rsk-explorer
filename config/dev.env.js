11b478ac9ce39c5934ee54be9b889bf443c03616
emi <emiliorizzo@gmail.com>
2018-05-19 20:20:26 -0300

    Update config/dev.env

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,9 +3,9 @@ const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 var cfg = require('../src/config/config.json')
 cfg = cfg.dev
-
+const WS_URL = process.env.WS_URL || cfg.WS_URL
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  WS_URL: '"' + cfg.WS_URL + '"',
+  WS_URL: '"' + WS_URL + '"',
 })
