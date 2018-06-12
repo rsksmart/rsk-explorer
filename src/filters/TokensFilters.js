f6a577d12d63017ed84b98674f062a4e9bf79bf9
emi <emiliorizzo@gmail.com>
2018-06-12 11:18:07 -0300

    Update tokensFilters

--- src/filters/TokensFilters.js
+++ src/filters/TokensFilters.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
-import BigNumber from 'bignumber.js'
+import { BigNumber } from 'bignumber.js'
 import etherUnits from '../lib/js/EtherUnits'
 // const Ether = new BigNumber(10e17)
 
 export const tokenAmount = (amount, decimals = 18) => {
   if (!amount) return
-  let ret = bignumberObjtoBigNumber(amount)
+  let ret = newBigNumber(amount)
   let divisor = new BigNumber(10).toPower(decimals)
   return ret.dividedBy(divisor).toString()
 }
@@ -16,25 +16,30 @@ export const tokenValue = Vue.filter('token-value', amount => {
 })
 
 export const txValue = Vue.filter('tx-value', value => {
-  value = bignumberObjtoBigNumber(value)
+  value = newBigNumber(value)
   return (value) ? etherUnits.toEther(value, 'wei') : 0
 })
 
 export const txGasPrice = Vue.filter('tx-gas-price', value => {
-  value = bignumberObjtoBigNumber(value)
+  value = newBigNumber(value)
   return (value) ? etherUnits.toEther(value, 'wei') : 0
 })
 
 export const bignumber = Vue.filter('big-number', value => {
   if (!value) return
-  return bignumberObjtoBigNumber(value).toString()
+  return newBigNumber(value).toString()
 })
 
-export const bignumberObjtoBigNumber = value => {
-  if (!value) return
-  let bn = new BigNumber(0)
-  bn.c = value.c
-  bn.e = value.e
-  bn.s = value.s
-  return bn
+export const newBigNumber = value => {
+  if (value !== '') {
+    if (typeof value === 'string') return new BigNumber(value)
+    if (typeof value === 'object' && undefined !== value.c && undefined !== value.e && undefined !== value.s) {
+      let bn = new BigNumber(0)
+      bn.c = value.c
+      bn.e = value.e
+      bn.s = value.s
+      return bn
+    }
+  }
+  return value
 }
