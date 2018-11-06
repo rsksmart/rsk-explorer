c4d90387be62e1f243d36d32b20bb999fe67e1fc
emi <emiliorizzo@gmail.com>
2018-11-06 13:24:01 -0300

    Update content.footer
    - Update copyright
    - Remove RSK public key

--- src/config/content.js
+++ src/config/content.js
@@ -1,6 +1,5 @@
 export default {
   'footer': [
-    'Copyright © 2015-2017 RSK Labs. All rights reserved.',
-    'RSK Public Key (1310 29B2 D95E 815A 48DA B443 FD4F DAFD 7D17 4BB2)'
+    'Copyright © 2015-2018 RSK Labs. All rights reserved.'
   ]
 }
