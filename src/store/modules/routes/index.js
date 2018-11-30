06234fcced76b35e6948fb98276f7d5523e12947
emi <emiliorizzo@gmail.com>
2018-11-30 19:53:55 -0300

    Update store/routes remove unused modules

--- src/store/modules/routes/index.js
+++ src/store/modules/routes/index.js
@@ -1,11 +1,9 @@
 import state from './state.js'
-import * as mutations from './mutations.js'
 import * as actions from './actions.js'
 import * as getters from './getters.js'
 
 export default {
   state,
-  mutations,
   actions,
   getters
 }
