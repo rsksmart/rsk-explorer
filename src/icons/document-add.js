a487384205d64d0f847431de2976dc7eecfc4194
emi <emiliorizzo@gmail.com>
2019-08-23 08:30:45 +0000

    Update icons

---
+++ src/icons/document-add.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'document-add': {
+    width: 640,
+    height: 640,
+    viewBox: '0 0 640 640',
+    data: '<path pid="0" d="M288 320v-64h64v64h64v64h-64v64h-64v-64h-64v-64h64zM128 576h384V192H384V64H128v512zm-64 32V0h384l128 128v512H64v-32z"/>'
+  }
+})
---
+++ src/icons/plus.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'plus': {
+    width: 512,
+    height: 512,
+    viewBox: '0 0 512 512',
+    data: '<path pid="0" d="M512 192H320V0H192v192H0v128h192v192h128V320h192z"/>'
+  }
+})
