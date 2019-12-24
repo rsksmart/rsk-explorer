e2c0d9d2bd5e7401087c6099a4bb330493309d32
emi <emiliorizzo@gmail.com>
2019-12-24 06:33:36 +0100

    Update components, add StatsBar & BoxField

---
+++ src/components/BoxField.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  .box-field.box
+    .box-field-icon
+      field-icon(:icon='field.icon')
+    .box-field-content
+      field-title(:field='field' :options='{hideIcon:true}')
+      data-field(:field='field' :row='row')
+</template>
+
+<script>
+import DataField from './DataField'
+import FieldTitle from './FieldTitle'
+import FieldIcon from './FieldIcon'
+export default {
+  name: 'box-field',
+  components: {
+    DataField,
+    FieldTitle,
+    FieldIcon
+  },
+  props: {
+    row: {
+      required: true
+    },
+    field: {
+      required: true
+    }
+  }
+}
+</script>
+<style lang="stylus">
+  @import '../lib/styl/style.styl'
+  @import '../lib/styl/mixins.styl'
+  .box-field.box
+    display flex
+    flex-flow row nowrap
+    .data-field, .field-title .title
+      margin 0
+    .box-field-content
+      margin 0 0 0 2em
+      .field-title
+        justify-content flex-start
+    .box-field-icon
+      flex-centered()
+      .svg-icon
+        width 2em
+        height @width
+</style>
---
+++ src/components/StatsBar.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .stats-bar.container
+    .cols
+      .col-a
+        .cols
+          //- first box
+          .col-a
+            box-field(:field='fields.hashrate' :row='stats')
+          //- second box
+          .col-b
+            box-field(:field='fields.circulatingSupply' :row='stats')
+      .col-b
+        .cols
+          //- third box
+          .col-a
+            box-field(:field='fields.bridgeBalance' :row='stats')
+          //- fourth box
+          .col-b
+            box-field(:field='fields.activeAccounts' :row='stats')
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import dataMixin from '../mixins/dataMixin'
+import BoxField from './BoxField'
+export default {
+  name: 'stats-bar',
+  mixins: [dataMixin],
+  components: {
+    BoxField
+  },
+  data () {
+    return {
+      type: 'stats'
+    }
+  },
+  computed: {
+    ...mapState({
+      stats: state => state.backend.stats
+    })
+  }
+}
+</script>
