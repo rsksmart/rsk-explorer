c5b3a899fc03d198bce836c82a7afa5a6c106f33
emi <emiliorizzo@gmail.com>
2019-12-27 16:55:17 -0300

    Update StatBar to new stats model

--- src/components/StatsBar.vue
+++ src/components/StatsBar.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .stats-bar.container
-    .cols
+    .cols(v-if='stats')
       .col-a
         .cols
           //- first box
@@ -8,12 +8,12 @@
             box-field(:field='fields.hashrate' :row='stats')
           //- second box
           .col-b
-            box-field(:field='fields.circulatingSupply' :row='stats')
+            box-field(:field='fields.circulatingSupply' :row='circulating')
       .col-b
         .cols
           //- third box
           .col-a
-            box-field(:field='fields.bridgeBalance' :row='stats')
+            box-field(:field='fields.bridgeBalance' :row='circulating')
           //- fourth box
           .col-b
             box-field(:field='fields.activeAccounts' :row='stats')
@@ -37,7 +37,10 @@ export default {
   computed: {
     ...mapState({
       stats: state => state.backend.stats
-    })
+    }),
+    circulating () {
+      return this.stats.circulating || {}
+    }
   }
 }
 </script>
