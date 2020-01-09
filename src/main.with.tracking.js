d55c90dcbad5320f29726bace5bffbe2373d343b
emi <emiliorizzo@gmail.com>
2020-01-09 17:20:31 +0100

    Fix main.with.tracking, remove console

--- src/main.with.tracking.js
+++ src/main.with.tracking.js
@@ -3,7 +3,6 @@ import VueGtag from 'vue-gtag'
 
 const { router } = config
 const id = process.env['GA_TAG']
-console.log(process.env)
 if (!id) throw new Error('Missing Google Analytics tag')
 VueI.use(VueGtag, {
   config: { id }
