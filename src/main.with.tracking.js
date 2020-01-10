fa22af22ac46241f80bba21e95de2d580b07d8ab
emi <emiliorizzo@gmail.com>
2020-01-10 19:42:04 +0100

    Replace v-gtag by vue-gtm

--- src/main.with.tracking.js
+++ src/main.with.tracking.js
@@ -1,11 +1,9 @@
 import { VueI, config } from './vue.instance'
-import VueGtag from 'vue-gtag'
+import VueGtag from 'vue-gtm'
 
 const { router } = config
 const id = process.env['GA_TAG']
 if (!id) throw new Error('Missing Google Analytics tag')
-VueI.use(VueGtag, {
-  config: { id }
-}, router)
+VueI.use(VueGtag, { id, vueRouter: router })
 /* eslint-disable no-new */
 new VueI(config).$mount('#app')
