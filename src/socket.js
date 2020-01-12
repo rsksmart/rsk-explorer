16da00fb2e361abcd642d3dca62b9787f8fdfbf0
emi <emiliorizzo@gmail.com>
2020-01-12 16:41:00 +0100

    Update socket.io configuration, try to use ws transport first

--- src/socket.js
+++ src/socket.js
@@ -1,3 +1,6 @@
 const io = require('socket.io-client')
-const socket = io.connect(process.env.WS_URL)
+const socket = io(process.env.WS_URL, { transports: ['websocket'] })
+socket.on('reconnect_attempt', () => {
+  socket.io.opts.transports = ['polling', 'websocket']
+})
 export default socket
