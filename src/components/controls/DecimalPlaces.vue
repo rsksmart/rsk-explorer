660d369a5e9f9583b57513f0043adbb3084afa19
emi <emiliorizzo@gmail.com>
2020-11-26 16:46:45 -0700

    Update components, add DecimalPlaces

---
+++ src/components/controls/DecimalPlaces.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  .decimal-places
+    input( :value='decimals' @change='updateDecimals' size="2")
+</template>
+<script>
+import { mapState, mapActions } from 'vuex'
+export default {
+  name: 'decimal-places',
+  computed: {
+    ...mapState({
+      decimals: state => state.config.decimalPlaces
+    })
+  },
+  methods: {
+
+    ...mapActions(['updateDecimalPlaces']),
+    updateDecimals (event) {
+      const { value } = event.target
+      return this.updateDecimalPlaces(value)
+    }
+  }
+}
+</script>
