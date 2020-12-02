40838d65982ee01653bd0d2714e6b69344972211
emi <emiliorizzo@gmail.com>
2020-12-02 12:13:43 -0700

    Refactor ExportFormat, use CtrlSwitchValues

--- src/components/controls/ExportFormat.vue
+++ src/components/controls/ExportFormat.vue
@@ -1,41 +1,23 @@
 <template lang="pug">
-.export-format.row
-  small.label(:class='formatCss("json")') json
-  ctrl-switch(:value="isCsv" @change='changeFormat' :square='true' css='enabled')
-  small.label(:class='formatCss("csv")') csv
+.export-format
+  ctrl-switch-values(:values="['json','csv']" :selected="format.toLowerCase()" @change="changeFormat")
 </template>
 <script>
-import { mapState, mapActions, mapGetters } from 'vuex'
-import CtrlSwitch from './CtrlSwitch'
+import { mapState, mapActions } from 'vuex'
+import CtrlSwitchValues from './CtrlSwitchValues'
 export default {
   name: 'export-format',
-  components: { CtrlSwitch },
+  components: { CtrlSwitchValues },
   computed: {
     ...mapState({
       format: state => state.config.exportFormat
-    }),
-    ...mapGetters({
-      isCsv: 'isCsvExport'
     })
   },
   methods: {
     ...mapActions(['updateExportFormat']),
-    switchFormat (format) {
-      return (format === 'CSV') ? 'JSON' : 'CSV'
-    },
-    changeFormat () {
-      const newFormat = this.switchFormat(this.format)
-      return this.updateExportFormat(newFormat)
-    },
-    formatCss (value) {
-      const format = this.format.toLowerCase()
-      return (value === format) ? ['brand', 'bold'] : 'disabled'
+    changeFormat (newFormat) {
+      return this.updateExportFormat(newFormat.toUpperCase())
     }
   }
 }
 </script>
-<style lang="stylus">
-  .export-format
-    .switch
-      margin 0 0.5em
-</style>
