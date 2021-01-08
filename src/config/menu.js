6de45e9793af1ef04d95a63bf80ce91b32015132
emi <emiliorizzo@gmail.com>
2021-01-08 10:48:53 -0700

    Change menu, add item options

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,8 +1,27 @@
-export default [
+
+import { ROUTES as r } from '../config/types'
+
+const makeMenuItem = (key, { title, icon, path } = {}) => {
+  title = (undefined === title) ? key : title
+  icon = (undefined === icon) ? key : icon
+  path = path || r[key]
+  return { key, title, icon, path }
+}
+
+const parseMenu = menu => menu.map(m => {
+  const key = (typeof m === 'string') ? m : m.key
+  const args = (typeof m === 'object') ? m : {}
+  return makeMenuItem(key, args)
+})
+
+const items = [
   'blocks',
   'transactions',
   'addresses',
   'tokens',
   'stats',
-  'settings'
+  { key: 'settings', title: false }
 ]
+export const menuItems = parseMenu(items)
+
+export default menuItems
